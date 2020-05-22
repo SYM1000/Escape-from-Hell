4f13366355445d4f2ae4d832e4484fdc02ee8a29
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2020-05-21 20:04:09 -0700

    Morge added, Secret room added

---
+++ Assets/IralHospital.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IralHospital : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        print("Cambio de escena al hospital");
+        SceneManager.LoadScene("Hospital");
+    }
+}
