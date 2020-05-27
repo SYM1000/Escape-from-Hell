0c2e4f327434d25efa6a3fa1f94a8c4e541b83c8
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2020-05-27 16:09:11 -0700

    Walking sound and camera filmic efects added

--- Assets/footsteps.cs
+++ Assets/footsteps.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class footsteps : MonoBehaviour
 {
-    
+    CharacterController cc;
+    AudioSource audio;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      cc = GetComponent<CharacterController>();
+      audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+
+        if(cc.isGrounded == true && cc.velocity.magnitude > 1f && audio.isPlaying == false && cc.velocity.magnitude < 4f){
+
+            audio.volume = Random.Range(0.8f , 1);
+            audio.pitch = Random.Range(0.8f, 1.1f);
+
+            audio.Play();
+        }else if(cc.isGrounded == true && cc.velocity.magnitude > 4f && audio.isPlaying == false){
+            audio.volume = Random.Range(0.8f , 1);
+            audio.pitch = Random.Range(1.45f, 2f);
+
+            audio.Play();
+        }
         
     }
 }
