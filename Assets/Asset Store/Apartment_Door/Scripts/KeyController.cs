29ef10342add6a698a6614783766ceb4d66cf421
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2020-06-07 18:33:29 -0700

    last

--- Assets/Asset Store/Apartment_Door/Scripts/KeyController.cs
+++ Assets/Asset Store/Apartment_Door/Scripts/KeyController.cs
@@ -1,34 +1,34 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿// using UnityEngine;
+// using UnityEngine.UI;
 
-//Script on Drag and drop will create box collider component automatically
-[RequireComponent(typeof(BoxCollider))]
-public class KeyController : MonoBehaviour
-{
-    BoxCollider keyCollider;
-    Rigidbody keyRB;
-    public Text txtToDisplay;
-    public DoorController DC;
+// //Script on Drag and drop will create box collider component automatically
+// [RequireComponent(typeof(BoxCollider))]
+// public class KeyController : MonoBehaviour
+// {
+//     BoxCollider keyCollider;
+//     Rigidbody keyRB;
+//     public Text txtToDisplay;
+//     //public DoorController DC;
 
-    /// <summary>
-    /// Incase user forgets to uncheck isTrigger in box collider
-    /// This sets them automatically
-    /// </summary>
-    private void Start()
-    {
-        keyCollider = GetComponent<BoxCollider>();
+//     /// <summary>
+//     /// Incase user forgets to uncheck isTrigger in box collider
+//     /// This sets them automatically
+//     /// </summary>
+//     private void Start()
+//     {
+//         //keyCollider = GetComponent<BoxCollider>();
 
-        keyCollider.isTrigger = true;
-    }
+//         //keyCollider.isTrigger = true;
+//     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            DC.gotKey = true;
-            txtToDisplay.gameObject.SetActive(true);
-            txtToDisplay.text = "Key Acquired";
-            this.gameObject.SetActive(false);
-        }
-    }
-}
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if (other.CompareTag("Player"))
+//         {
+//             DC.gotKey = true;
+//             txtToDisplay.gameObject.SetActive(true);
+//             txtToDisplay.text = "Key Acquired";
+//             this.gameObject.SetActive(false);
+//         }
+//     }
+// }
